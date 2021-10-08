5da047de14ed315a7d122eb82f9a238b5b97d4d3
mm96249624 <89981035+mm96249624@users.noreply.github.com>
2021-10-08 23:27:43 +0400

    Initial commit

---
+++ .idea/as.py
@@ -0,0 +1,17 @@
+import HMS
+
+def test_measure_pulse():
+    a = HMS.measure_pulse(40,50,60)
+    assert (a != "negative")
+
+def test_abnormal():
+    a = HMS.abnormal(60,70,90,100)
+    assert(a == none)
+
+import main.py
+
+stub = stubout.StubOutForTesting()
+stub.Set(measure_pulse, (r1, r2, r3))
+
+
+
